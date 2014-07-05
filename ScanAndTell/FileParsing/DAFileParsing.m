54f24588bb8233ec9c9173c43c29fc43f7251455
suman chatterjee <suman.chatterjee@prometheanworld.com>
2014-07-05 09:57:05 +0100

    commit bug fixes for sliding menu

--- ScanAndTell/FileParsing/DAFileParsing.m
+++ ScanAndTell/FileParsing/DAFileParsing.m
@@ -15,35 +15,20 @@
 
 - (void) parseFileInContext:(NSManagedObjectContext*)context
 {
-    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"pp-2012" ofType:@"csv"];
- //   NSString *csvString = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
-    NSArray *locations = [NSArray arrayWithContentsOfCSVFile:filePath options:CHCSVParserOptionsRecognizesBackslashesAsEscapes];
-    
-    
-//    NSArray *locations = [csvString componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
-    
-    for (NSArray * location in locations)
-    {
-        NSLog(@"count %lu",(unsigned long)location.count);
-//        if([location isEqualToString:@""])
-//            continue;
-  //      NSLog(@"String is %@", location[3]);
-   //     NSString *removeComma = [[location componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]] componentsJoinedByString:@","];
-   //     NSLog(@"String is %@", removeComma);
-
-    //    NSArray *components = [location componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"\\,"]];
-     //   [location componentsSeparatedByString:@","];
-        //NSLog(@"array is %@ %@", com6ponents[1],components[3]);
-     //   if(components.count == 0 )
-    //        continue;
-        if(location != nil && location.count > 1){
-            NSLog(@"String is %@", location[3]);
-            [self storeInDatabase:location inContext:context];
-        }
-        
-        
-    }
-    
+//    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"pp-2012" ofType:@"csv"];
+//    NSArray *locations = [NSArray arrayWithContentsOfCSVFile:filePath options:CHCSVParserOptionsRecognizesBackslashesAsEscapes];
+//    
+//    for (NSArray * location in locations)
+//    {
+//        NSLog(@"count %lu",(unsigned long)location.count);
+//        if(location != nil && location.count > 1){
+//            NSLog(@"String is %@", location[3]);
+//            [self storeInDatabase:location inContext:context];
+//        }
+//        
+//        
+//    }
+//    
 }
 
 
